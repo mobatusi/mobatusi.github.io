c2d696db46a739e29431e0035e965ebb7ebe9817
Dolu <mobatusi@gmail.com>
2021-04-09 11:03:31 -0400

    build successful

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-nprogress/gatsby-browser.js'),
+      options: {"plugins":[],"color":"tomato","showSpinner":true},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/pages/about.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/pages/blog.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/pages/index.js"))),
+  "component---src-pages-projects-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/pages/projects.js"))),
+  "component---src-pages-resume-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/pages/resume.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/templates/blog-post.js"))),
+  "component---src-templates-projects-post-js": hot(preferDefault(require("/Users/mosadoluwaobatusin/Documents/Github/mobatusi.github.io/src/templates/projects-post.js")))
+}
+
